8f11a262e8ea9ebe4686a77e9af50c4eb8865543
Mitch Strange <mstrange382@gmail.com>
2022-03-08 21:52:47 -0600

    installed material ui

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
+        hello world
         
       </header>
     </div>
